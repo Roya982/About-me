6af7125b5df865dc3bd734f825bfc9cdde018f3f
Roya982 <roayarana@gmail.com>
2021-02-08 18:44:56 +0200

    doing assigment 2

---
+++ js/main.js
@@ -0,0 +1,77 @@
+'use strict';
+let letter = prompt ('Does your name starts with the letter\"R\"?');
+switch (letter.toLowerCase()){
+    case ('yes'):
+        alert ('We have the same first letter!');
+    break;
+    case ('y') :
+        alert ('We have the same first letter!');
+    break;    
+    case  ('no'):
+        alert ('It\'s ok, your first letter is pretty to<3');
+    break;
+    case ('n'):
+        alert ('It\'s ok, your first letter is pretty to<3');
+}
+let myAge = prompt('Are you 23 years old?');
+switch ( myAge.toLowerCase()){
+    case ('yes'):
+        alert ('we\'re the same age!');
+    break;
+    case ( 'y'):
+        alert ('we\'re the same age!');
+    break;
+    case ('no'):
+        alert ('We\'re not the same age :\'(');
+    break;
+    case ('n'):
+        alert ('We\'re not the same age :\'(');
+    break;
+}
+let intrest = prompt('Are you intrested in programming?');
+switch (intrest.toLowerCase()){
+    case ('yes'):
+        alert ('Great, me to!');
+    break;
+    case ('y'):
+        alert ('Great,me to!');
+    break;
+    case ('no'):
+        alert ('sorry to hear that');
+    break;
+    case ('n'):
+        alert ('sorry to hear that');
+    break;
+}
+let anime = prompt ('Do you like anime?');
+switch (anime.toLowerCase()){
+    case ('yes'):
+        alert ('OMG! me too,greeting anime lover fellow<3');
+    break;
+    case ('y'):
+        alert ('OMG! me too,greeting anime lover fellow<3');
+    break;
+    case ('no'):
+        alert ('It\'s ok, I\'m sure you have other intrests.');
+    break;
+    case ('n'):
+        alert ('It\'s ok, I\'m sure you have other intrests.')
+    break;
+}
+let reading = prompt ('Do you like reading?');
+switch (reading.toLowerCase()){
+    case ('yes'):
+        alert ('It\'s grat to meet another reader! we\'re rare those days');
+    break;
+    case ('y'):
+        alert ('It\'s grat to meet another reader! we\'re rare those days');
+    break;
+    case ('no'):
+        alert ('You should try to read more, it\'s a lot of fun and benifit');
+    break;
+    case ('n'):
+        alert ('You should try to read more, it\'s a lot of fun and benifit');
+    break;
+}
+let userName = prompt ('what\'s you\'re name?')
+alert ( userName + ' welcome here I wish you will enjoy this place');
