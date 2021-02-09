f48de655ce01ddf4258d7050d2c695abdfc8fc75
Roya982 <roayarana@gmail.com>
2021-02-09 22:08:15 +0200

    lab3 edit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 'use strict';
-let userName = prompt ('what\'s you\'re name?');
-let letter = prompt ('Does your name starts with the letter\'R\'?');
+let score=0;
+let userName = prompt ("what's you're name?");
+let letter = prompt ("Does your name starts with the letter'R'?");
 switch (letter.toLowerCase()){
 case ('yes'):
 case ('y') :
   alert ('We have the same first letter!');
+  score+=1;
   break;
 case ('no'):
 case ('n'):
@@ -16,6 +18,7 @@ switch ( myAge.toLowerCase()){
 case ('yes'):
 case ( 'y'):
   alert ('we\'re the same age!');
+  score+=1;
   break;
 case ('no'):
 case ('n'):
@@ -27,6 +30,7 @@ switch (intrest.toLowerCase()){
 case ('yes'):
 case ('y'):
   alert ('Great,me to!');
+  score+=1;
   break;
 case ('no'):
 case ('n'):
@@ -38,6 +42,7 @@ switch (anime.toLowerCase()){
 case ('yes'):
 case ('y'):
   alert ('OMG! me too,greeting anime lover fellow<3');
+  score+=1;
   break;
 case ('no'):
 case ('n'):
@@ -49,6 +54,7 @@ switch (reading.toLowerCase()){
 case ('yes'):
 case ('y'):
   alert ('It\'s grat to meet another reader! we\'re rare those days');
+  score+=1;
   break;
 case ('no'):
 case ('n'):
@@ -68,6 +74,7 @@ while(tries && !right){
     if(userNumber === favouritNumber[0]){
       alert('Great!. You\'re right, my favourit number is 2');
       console.log(userNumber);
+      score+=1;
       right=true;
       break;
     }
@@ -80,3 +87,29 @@ while(tries && !right){
     alert('so bad:\'(, my favourit number is 2');
   }
 }
+
+let topTen=['Bleach','FMAB','BNHA','Didimon Adventure','Dragon Quest','NARUTO','Fate series','Demon Slayer','JJK'];
+let attempts=6;
+let favourit=false;
+alert('try to guess my favourit 1 top anime, be aware that you have just 6 attempts!');
+let userAnswer=prompt('my faviourt anime name is the main character\'s name');
+let character =['Akame ga kill', 'Koroku no baskit','Dororo','Durarara!!'];
+while(attempts && !favourit){
+  for(let f=0;f<topTen.length;f++){
+    if(userAnswer.toUpperCase() === topTen[5]){
+      alert('fantastic!. You guessed that right, my favourit Anime is Naruto');
+      console.log(userAnswer);
+      score+=1;
+      favourit=true;
+      break;
+    }
+    attempts-=1;
+    userAnswer= prompt ('You\'re wrong:\'(, it\'s ok you can still try '+attempts+' time/s');
+  }
+  if(favourit){
+    break;
+  }else{
+    alert('you could\'nt figure it out and that made me sad:\'(, my favourit number is Naruto, but here another animes I like have the name of it\'s main character: '+character);
+  }
+}
+alert('we have '+score+' similarities!');
